98805cfe7e232bece73b1a52d5676f55baf23a1f
ale-mikhailova <gvenvivarr@yandex.ru>
2024-01-18 17:13:20 +0300

    test file changed

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+
+class TestFindButton():
+    def test_find_bucket_button(self, browser):
+        browser.get("https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+        assert len(browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")) > 1, "Кнопка добавления в корзину отсутствует"
+
+
