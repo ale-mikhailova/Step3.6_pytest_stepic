59eed3d2caaad2cbcc53886eb6df95dcdd180231
ale-mikhailova <gvenvivarr@yandex.ru>
2024-01-18 17:20:46 +0300

    final changes

--- test_items.py
+++ test_items.py
@@ -1,8 +1,9 @@
 import time
+from selenium.webdriver.common.by import By
 
 class TestFindButton():
     def test_find_bucket_button(self, browser):
         browser.get("https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-        assert len(browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")) > 1, "Кнопка добавления в корзину отсутствует"
+        assert len(browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")) == 1, "Кнопка добавления в корзину отсутствует"
 
 
